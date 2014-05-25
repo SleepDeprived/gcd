747a50218f25d0c14ae21223d75e6c1c59dab788
Tom Metzger <tom@tmetzger.com>
2014-05-25 14:56:26 -0400

    created runPeerAnalysis() function that wraps all data manipulation steps in one function that can be called to produce the output, result is gcd_peer_tidy_data.txt

--- run_analysis.R
+++ run_analysis.R
@@ -2,30 +2,35 @@
 library(plyr)
 library(reshape2)
 
-columns <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE)
 
-xtest <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
-ytest <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
-subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+runPeerAnalysis <- function(){ 
+  columns <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE)
 
-xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
-ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
-subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+  xtest <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
+  ytest <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+  subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
 
-test <- cbind(subjectTest, ytest, xtest)
-colnames(test)[1:2] <- c("subject","activity")
-train <- cbind(subjectTrain, ytrain, xtrain)
-colnames(train)[1:2] <- c("subject","activity")
+  xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
+  ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+  subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
 
-data <- rbind(test, train)
+  test <- cbind(subjectTest, ytest, xtest)
+  colnames(test)[1:2] <- c("subject","activity")
+  train <- cbind(subjectTrain, ytrain, xtrain)
+  colnames(train)[1:2] <- c("subject","activity")
 
-measurementMean <- rowMeans(data[,3:563], )
-stdDev <- apply(data[,3:563], MARGIN = 1, sd)
-activity <- factor(as.character(data$activity), labels = columns$V2)
+  data <- rbind(test, train)
 
-allSubjectActivity <- data.frame(data$subject, activity, measurementMean, stdDev)
-colnames(allSubjectActivity)[1] <- "subject"
+  measurementMean <- rowMeans(data[,3:563], )
+  stdDev <- apply(data[,3:563], MARGIN = 1, sd)
+  activity <- factor(as.character(data$activity), labels = columns$V2)
 
-m <- melt(allSubjectActivity, id = c("subject", "activity"), measure = c("measurementMean"))
+  allSubjectActivity <- data.frame(data$subject, activity, measurementMean, stdDev)
+  colnames(allSubjectActivity)[1] <- "subject"
 
-tidyDataSet <- dcast(m, subject + activity ~ variable, mean)
+  m <- melt(allSubjectActivity, id = c("subject", "activity"), measure = c("measurementMean"))
+
+  tidyDataSet <- dcast(m, subject + activity ~ variable, mean)
+
+  write.table(tidyDataSet, file = "gcd_peer_tidy_data.txt", sep = "\t")
+}
